1d0741317b9ce73f640bb351fc3db4048da17f48
faizan <faizan.yaseen@techcologic.com>
2022-03-26 23:21:36 +0500

    new pdf experience added

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,7 @@
 class ReportsController < ApplicationController
     before_action :authenticate_user!
     before_action :set_authorizer
+    before_action :set_printing_reports, only: [:download_experience, :download_report]
     def index
         @credentials = @authorizer.get_credentials @user_id
         if @credentials.blank?
@@ -10,6 +11,7 @@ class ReportsController < ApplicationController
             end
         end
         if @credentials.present?
+            redirect_to connect_google_sheets_path if @credentials.expires_at < Time.now
             service = Google::Apis::SheetsV4::SheetsService.new
             service.client_options.application_name = ENV['APPLICATION_NAME']
             service.authorization = @credentials
@@ -19,7 +21,7 @@ class ReportsController < ApplicationController
             @found_tags = []
             response.values.each do |row|
                 row[1].to_s.split(/[,;\s]/).each {|tag| @found_tags << tag unless tag.in?(@found_tags)}
-                report = Report.new(row[1], row[0], row[4], row[3], row[9], row[21], row[10] ,row[11] ,row[13] ,row[12] ,row[15] ,row[14] ,row[7] ,row[5] ,row[6])
+                report = Report.new(row[1], row[0], row[4], row[3], row[9], row[21], row[10] ,row[11] ,row[13] ,row[12] ,row[15] ,row[14] ,row[7] ,row[5] ,row[6], row[8])
                 @@reports << report
             end
         end
@@ -29,43 +31,67 @@ class ReportsController < ApplicationController
         if credentials.nil?       
             url = @authorizer.get_authorization_url base_url: ENV['OOB_URI']
             redirect_to url
+        elsif credentials.present? && credentials.expires_at < Time.now
+            tokens = YAML.load_file(ENV["TOKEN_PATH"])
+            tokens.delete('default')
+            File.open(ENV["TOKEN_PATH"], "w"){|f| YAML.dump(tokens, f)}
+            url = @authorizer.get_authorization_url base_url: ENV['OOB_URI']
+            redirect_to url
         else
             redirect_to root_path
         end
     end
     #a b c d e f g h i j k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  Z
     #0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
+    def download_experience
+        
+    end
+
     def download_report
-        tags = params[:tags]
-        @printing_reports = []
-        @@reports.each do |report|
-            report_tags = report.get_tags
-            report_tags.each do |report_tag|
-                if report_tag.in?(tags)
-                    @printing_reports << report
-                    break
+        if params[:download_type] == 'reports'
+            respond_to do |format|
+                format.pdf do
+                    @pdf = render_to_string(
+                    pdf: "file_name.pdf",
+                    template: "reports/show.html.erb",
+                    #locals:  {report: @@reports.first},
+                    layout: 'pdf.html.erb',
+                    page_size: 'Letter',
+                    # header: {html: {template: 'reports/_header'}},
+                    footer: {html: {template: 'reports/_footer'}},
+                    show_as_html: true,
+                    margin:  {  top:               0,                     # default 10 (mm)
+                                left:              0,
+                                right:             0})
+    
+                    send_data(@pdf, filename: "projects_report.pdf")
+                end
+                format.html do 
+                    render "reports/show.html.erb"
                 end
             end
-        end
-        respond_to do |format|
-            format.pdf do
-                @pdf = render_to_string(
-                pdf: "file_name.pdf",
-                template: "reports/show.html.erb",
-                #locals:  {report: @@reports.first},
-                layout: 'pdf.html.erb',
-                page_size: 'Letter',
-                # header: {html: {template: 'reports/_header'}},
-                footer: {html: {template: 'reports/_footer'}},
-                show_as_html: true,
-                margin:  {  top:               0,                     # default 10 (mm)
-                            left:              0,
-                            right:             0})
-
-                send_data(@pdf, filename: "projects_report.pdf")
-            end
-            format.html do 
-                render "reports/show.html.erb"
+        elsif params[:download_type] == 'experience'
+            respond_to do |format|
+                format.pdf do
+                    @pdf = render_to_string(
+                    pdf: "file_name.pdf",
+                    template: "reports/experience.html.erb",
+                    #locals:  {report: @@reports.first},
+                    layout: 'pdf.html.erb',
+                    page_size: 'A4',
+                    orientation: 'Landscape',
+                    header: {html: {template: 'reports/_header'}},
+                    footer: {html: {template: 'reports/_footer'}},
+                    show_as_html: true,
+                    margin:  {  top:               42,         # default 10 (mm)
+                                left:              0,#16,
+                                right:            0})#20})
+    
+                    send_data(@pdf, filename: "previous_project_experiences.pdf")
+                end
+                format.html do 
+                    render "reports/show.html.erb"
+                end
             end
         end
     end
@@ -77,5 +103,18 @@ class ReportsController < ApplicationController
         token_store = Google::Auth::Stores::FileTokenStore.new file: ENV['TOKEN_PATH']
         @authorizer = Google::Auth::UserAuthorizer.new client_id, scope, token_store
         @user_id = "default"
+    end
+    def set_printing_reports
+        tags = params[:tags]
+        @printing_reports = []
+        @@reports.each do |report|
+            report_tags = report.get_tags
+            report_tags.each do |report_tag|
+                if report_tag.in?(tags)
+                    @printing_reports << report
+                    break
+                end
+            end
+        end
     end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,5 @@
 class Report
-    def initialize(tags = "" ,project_no = "",project_name = "",client = "",region_location = "",client_contact_details = "",start_date = "",end_date = "",project_value_us = "",project_value_zar = "",contract_value_us = "",contract_value_zar = "",summary = "",description = "",scope_of_services = "")
+    def initialize(tags = "" ,project_no = "",project_name = "",client = "",region_location = "",client_contact_details = "",start_date = "",end_date = "",project_value_us = "",project_value_zar = "",contract_value_us = "",contract_value_zar = "",summary = "",description = "",scope_of_services = "", contract = "")
         @project_name =project_name
         @client =client
         @region_location = region_location
@@ -15,11 +15,12 @@ class Report
         @scope_of_services = scope_of_services
         @project_no = project_no
         @tags = tags
+        @contract = contract
     end
 
     def get_tags
         @tags.present? ? @tags.split(/[.;\s]/) : []
     end
 
-    attr_reader :tags, :project_no ,:project_name ,:client ,:region_location ,:client_contact_details ,:start_date ,:end_date ,:project_value_us ,:project_value_zar ,:contract_value_us ,:contract_value_zar ,:summary ,:description ,:scope_of_services
+    attr_reader :tags, :project_no ,:project_name ,:client ,:region_location ,:client_contact_details ,:start_date ,:end_date ,:project_value_us ,:project_value_zar ,:contract_value_us ,:contract_value_zar ,:summary ,:description ,:scope_of_services ,:contract
 end
