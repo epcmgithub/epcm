bb20fc4d2d94257e0229a98cb4bc258bf597fbfb
faizan <faizan.yaseen@techcologic.com>
2022-04-08 01:06:12 +0500

    experience added

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -46,7 +46,9 @@ class ReportsController < ApplicationController
     #a b c d e f g h i j k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  Z
     #0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
     def download_experience
-        
+        respond_to do |format|
+            format.docx { headers["Content-Disposition"] = "attachment; filename=\"experience.docx\"" }
+        end
     end
 
     def download_report
@@ -55,8 +57,9 @@ class ReportsController < ApplicationController
             # format.docx do
             #     respond_with(@object, filename: 'my_file.docx', word_template: 'coder.ott')
             # end
+            #redirect_to download_experience_path and return if params[:download_type] == 'experience'
             respond_to do |format|
-                format.docx { headers["Content-Disposition"] = "attachment; filename=\"caracal.docx\"" }
+                format.docx { headers["Content-Disposition"] = "attachment; filename=\"reports.docx\"" }
             end
         # end
         # if params[:download_type] == 'reports'
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'reports#index'
   get 'oauth2callback' => 'reports#index'
   get 'download_report' , to: 'reports#download_report' , as: :download_report
+  get 'download_experience' , to: 'reports#download_experience' , as: :download_experience
   get 'connect_google_sheets' , to: 'reports#connect_google_sheets' , as: :connect_google_sheets
   
 end
